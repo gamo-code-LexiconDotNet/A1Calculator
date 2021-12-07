eb1b47fd326e1da8399c12461c5a9410abc3d627
Gabriel Molander <gabriel.molander@gmail.com>
2021-12-07 13:45:11 +0100

    Basic functionality

---
+++ ReplCalculator/Calculator.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReplCalculator
+{
+    class Calculator
+    {
+        /**
+         * Evaluate an inifix notation expression
+         * 
+         * Algorithm from https://www.geeksforgeeks.org/expression-evaluation/
+         *  
+         * Added support for:
+         *  floating point values
+         *  power of
+         *  modulus
+         *  pi
+         */
+        public static double EvaluateInifix(string expression)
+        {
+            Stack<double> values = new Stack<double>();
+            Stack<char> ops = new Stack<char>();
+            
+            char[] tokens = expression.ToCharArray();
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (tokens[i] == ' ')
+                    continue;
+
+                if (tokens[i] == 'p' && tokens[i + 1] == 'i')
+                {
+                    values.Push(Math.PI);
+                    i++;
+                }
+
+
+                if ((tokens[i] >= '0' && tokens[i] <= '9') ||
+                    tokens[i] == '.')
+                {
+                    StringBuilder sbuf = new StringBuilder();
+
+                    while (i < tokens.Length &&
+                        ((tokens[i] >= '0' && tokens[i] <= '9') ||
+                        tokens[i] == '.'))
+                    {
+                        sbuf.Append(tokens[i++]);
+                    }
+                    values.Push(double.Parse(sbuf.ToString()));
+
+                    i--;
+                }
+                else if (tokens[i] == '(')
+                {
+                    ops.Push(tokens[i]);
+                }
+                else if (tokens[i] == ')')
+                {
+                    while (ops.Peek() != '(')
+                    {
+                        values.Push(ApplyOp(ops.Pop(),
+                                         values.Pop(),
+                                        values.Pop()));
+                    }
+                    ops.Pop();
+                }
+                else if (tokens[i] == '+' || tokens[i] == '-' ||
+                    tokens[i] == '*' || tokens[i] == '/' ||
+                    tokens[i] == '^' || tokens[i] == '%')
+                {
+                    while (ops.Count > 0 && 
+                        HasPrecedence(tokens[i], 
+                        ops.Peek()))
+                    {
+                        values.Push(ApplyOp(ops.Pop(),
+                                         values.Pop(),
+                                       values.Pop()));
+                    }
+
+                    ops.Push(tokens[i]);
+                }
+            }
+
+            while (ops.Count > 0)
+                values.Push(ApplyOp(ops.Pop(), values.Pop(), values.Pop()));
+
+            return values.Pop();
+        }
+
+        /**
+         * Included ^ and % in presidence order
+         * Order: ^, [*, /], [+, -], %, [(, )]
+         */
+        public static bool HasPrecedence(char op1, char op2)
+        {
+            // handle (, )
+            if (op2 == '(' || op2 == ')')
+                return false;
+
+            // handle %
+            if ((op2 == '%'))
+                return false;
+
+            //handle +, -
+            if ((op2 == '+' || op2 == '-') && (op1 == '*' || op1 == '/' || op1 == '^'))
+                return false;
+
+            //handle *, /
+            if ((op2 == '*' || op2 == '/') && (op1 == '^'))
+                return false;
+
+            // implicit true for all other cases
+            return true;
+        }
+
+        /**
+         * Added support for ^ and %
+         */
+        public static double ApplyOp(char op, double b, double a)
+        {
+            switch (op)
+            {
+                case '+':
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '*':
+                    return a * b;
+                case '/':
+                    if (b == 0)
+                    {
+                        throw new
+                        System.NotSupportedException(
+                               "Cannot divide by zero");
+                    }
+                    return a / b;
+                case '^':
+                    return Math.Pow(a, b);
+                case '%':
+                    return a % b;
+            }
+            return 0;
+        }
+    }
+}
+
---
+++ ReplCalculator/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using static ReplCalculator.Calculator;
+using static System.Console;
+
+namespace ReplCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            ClearScreen();
+
+            bool show = true;
+            while (show)
+                show = Repl();
+        }
+
+        private static bool Repl()
+        {
+            Write("> ");
+
+            string input = ReadLine();
+
+            if (input == "q")
+                return false;
+            else if (input == "h")
+                PrintHelpMessage();
+            else if (input == "c")
+                ClearScreen();
+            else
+                try
+                {
+                    WriteLine(Math.Round(EvaluateInifix(input), 4).ToString());
+                } catch
+                {
+                    WriteLine("Could not evaluate that expresson.");
+                }
+
+            return true;
+        }
+
+        private static void ClearScreen()
+        {
+            Clear();
+            WriteLine("[Lexicon C#/.Net Programming] Assignment 1 - Repl Calculator");
+            WriteLine("(type q to quit, h for help, c for clear screen)\n");
+        }
+
+        private static void PrintHelpMessage()
+        {
+            WriteLine("Type in a mathematical expression using the operators:\n" +
+                " ^: power of\n" +
+                " *: multiply\n" +
+                " /: division\n" +
+                " +: addition\n" +
+                " -: subtraction\n" +
+                " %: modulus\n" +
+                " pi: Pi\n" +
+                " (,): parenthesis\n" +
+                "Example:\n" +
+                "> 1+9^(1/2)%3\n" +
+                "1\n");
+        }
+    }
+}
